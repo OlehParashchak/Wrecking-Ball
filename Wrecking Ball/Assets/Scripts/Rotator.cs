b414e4b9ced0098b400de6b0e462eb692a4859d5
Oleh <139156765+OlehParashchak@users.noreply.github.com>
2023-08-25 14:58:58 +0300

    Wreking Ball

---
+++ Wrecking Ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed = 100;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+    }
+}
---
+++ Wrecking Ball/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    private AudioSource audioSource;
+    public bool sound = true;
+
+    private void Awake()
+    {
+        MakeSingleton();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void MakeSingleton()
+    {
+        if(instance != null)
+            Destroy(gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    public void SoundOnOff()
+    {
+        sound = !sound;
+    }
+
+    public void PlaySoundFX(AudioClip clip, float volume)
+    {
+        if (sound)
+            audioSource.PlayOneShot(clip, volume);
+    }
+}
